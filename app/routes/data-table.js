85d6ef27bd8717811d28f4588c80852149743aa5
Jay Port <usbportnoy@gmail.com>
2021-06-09 22:56:39 -0400

    Adding quotes

--- app/routes/data-table.js
+++ app/routes/data-table.js
@@ -18,7 +18,17 @@ export default class DataTableRoute extends Route {
                 name: "Bobby Hill",
                 quote: "I don't know you!",
                 altQuote: "To tell you the truth, Dad, that sounds boring"
-            }
+            },
+            {
+                name: "Luanne Platter",
+                quote: "No, Uncle Hank. Mama and Daddy are still fighting.",
+                altQuote: "Boy. Aunt Peg the words I don't know could fill a dictionary"
+            },
+            {
+                name: "Joseph Gribble",
+                quote: "Your mom is as cool as most peoples dads",
+                altQuote: "Sup."
+            },
         ];
     }
 }
