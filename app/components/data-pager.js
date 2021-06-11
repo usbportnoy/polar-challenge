76dba3bc99865b2d608221fb29b70fb15b047800
Jay Port <usbportnoy@gmail.com>
2021-06-11 15:43:56 -0400

    Setting default values

--- app/components/data-pager.js
+++ app/components/data-pager.js
@@ -4,13 +4,27 @@ import { action } from '@ember/object';
 
 export default class DataPager extends Component {
     @tracked page = 1;
-    @tracked pages = Math.ceil(this.args.data.length/this.args.pageSize);
 
-    @tracked pagedData = this.getData(this.args.data, this.page, this.args.pageSize)
+    @tracked pagedData = this.getData(this.data, this.page, this.pageSize)
     @tracked isFirst = this.page == 1;
     @tracked isLast = this.pages <= this.page;
     @tracked nextPage = this.page + 1;
     @tracked previousPage = this.page - 1;
+
+    pages = Math.ceil(this.args.data.length/this.pageSize);
+
+    get pageSize(){
+        return this.args.pageSize || 1;
+    }
+
+    get title(){
+        return this.args.title || null;
+    }
+
+    get data(){
+        return this.args.data || [];
+    }
+
 
     getData = function(content, page, max){
         return content.slice((page - 1) * max, page * max);
@@ -23,6 +37,6 @@ export default class DataPager extends Component {
         this.isLast = this.pages <= this.page;
         this.nextPage = this.page + 1;
         this.previousPage = this.page - 1;
-        this.pagedData = this.getData(this.args.data, this.page, this.args.pageSize)
+        this.pagedData = this.getData(this.data, this.page, this.pageSize)
     }
 }
